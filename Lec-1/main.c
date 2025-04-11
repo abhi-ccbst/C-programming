66e081cd2605c41908dd99855235d9d51f5c03d3
Abhi Patel <abhipatel@Abhis-MacBook-Pro.local>
2025-04-11 19:29:35 -0400

    Lec-1

---
+++ Lec-1/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // This is comment
+    /*
+    hgsDVAXMCV
+    DSFV
+    AVA
+    VAS
+    VA
+    VADV
+    */
+    printf("Hello world!\n \t \\ \" I am learning C."); //This is comment
+
+    return 0;
+}
