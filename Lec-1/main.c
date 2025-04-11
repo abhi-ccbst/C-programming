3dbcfcc3c49d4edd5fb94547a808b9d6b720e08e
Abhi Patel <abhipatel@Abhis-MacBook-Pro.local>
2025-04-11 19:32:31 -0400

    Lec-1

--- Lec-1/main.c
+++ Lec-1/main.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    // This is comment
-    /*
-    hgsDVAXMCV
-    DSFV
-    AVA
-    VAS
-    VA
-    VADV
-    */
-    printf("Hello world!\n \t \\ \" I am learning C."); //This is comment
+    printf("=====================================\n");
+    printf("\t\"Self Information Card\"\n");
+    printf("=====================================\n");
+
+    printf("Name\t\t: \"John Doe\"\n");
+    printf("Age\t\t: 25\n");
+    printf("Profession\t: \"Software Developer\"\n");
+    printf("Hobbies\t\t: \"Reading\\Coding\\Gaming\"\n");
+
+    printf("=====================================\n");
+    printf("\tThank you! \\(^_^)/\n");
+    printf("=====================================\n");
 
     return 0;
 }
