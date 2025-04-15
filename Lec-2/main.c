40231807d882e27344ce0954c06512155f320441
Abhi Patel <abhipatel@Abhis-MacBook-Pro.local>
2025-04-14 20:55:13 -0400

    Lec-2

--- Lec-2/main.c
+++ Lec-2/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
-
+#include <string.h>
 
 int main() {
     const int minutesPerHour = 60;
@@ -65,5 +65,46 @@ int main() {
         }
         printf("%d\n", i);
     }
+    int myNumbers[] = {25, 50, 100, 102};
+    printf("%d", sizeof(myNumbers)/sizeof(myNumbers[0]));   //4 bytes * 4  /4 bytes
+    for (i = 0; i < sizeof(myNumbers) / sizeof(myNumbers[0]); i++) {
+        if (i == 2) {
+            myNumbers[i] = 21;
+        }
+        printf("%d\n", myNumbers[i]);
+    }
+
+
+    // char greeting[] = "Hello World!";
+    char greeting[] = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!', '\0'};
+    printf("%s\n", greeting);
+
+    // int length = strlen(greeting);
+    // printf("Length: %d\n", length);
+
+    for (i = 0; i < strlen(greeting); i++) {
+        printf("%c\n", greeting[i]);
+    }
+
+    char str1[] = "Hello";
+    char str2[] = "Hello";
+    char str3[] = "Hi";
+    
+    // strcat(str1, str2); // str1 = str1 + str2
+    printf("%s", str1);
+    printf("%d\n", strcmp(str1, str2)); 
+    printf("%d\n", strcmp(str1, str3));
+
+    int myNum;
+    char myChar;
+
+    printf("Type a number AND a character and press enter: \n");
+
+    scanf("%d %c", &myNum, &myChar);
+
+    printf("Your number is: %d\n", myNum);
+
+    printf("Your character is: %c\n", myChar);
+
     return 0;
 }
